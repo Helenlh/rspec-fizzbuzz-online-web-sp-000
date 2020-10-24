85d10f0b264a8fb54e8aaeb5f11b4659484a5002
HELEN HARMAN <hlharman@bellsouth.net>
2020-10-24 21:51:17 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,7 +4,7 @@
 def fizzbuzz(number)
 if number % 3 == 0 && number % 5 == 0
   return "FizzBuzz"
-else number % 3 == 0 
+elsif number % 3 == 0 
    return "Fizz" 
   elsif number % 5 == 0 
    return "Buzz"  
